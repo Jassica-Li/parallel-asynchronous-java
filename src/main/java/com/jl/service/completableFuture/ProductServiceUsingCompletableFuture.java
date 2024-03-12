374b50a82a8acc33c1ea3a8b0c778c24c9f14cb6
JL <jling23@163.com>
2024-03-12 20:53:09 +0800

    jl feat: CompleatbleFuture and stream()

--- src/main/java/com/jl/service/completableFuture/ProductServiceUsingCompletableFuture.java
+++ src/main/java/com/jl/service/completableFuture/ProductServiceUsingCompletableFuture.java
@@ -1,39 +1,93 @@
 package com.jl.service.completableFuture;
 
 
-import com.jl.domain.Product;
-import com.jl.domain.ProductInfo;
-import com.jl.domain.Review;
+import com.jl.domain.*;
+import com.jl.service.InventoryService;
 import com.jl.service.ProductInfoService;
 import com.jl.service.ReviewService;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-import static com.jl.util.CommonUtil.stopWatch;
-import static com.jl.util.CommonUtil.timeTaken;
+import static com.jl.util.CommonUtil.*;
 import static com.jl.util.LoggerUtil.log;
 
 public class ProductServiceUsingCompletableFuture {
     private final ProductInfoService productInfoService;
     private final ReviewService reviewService;
 
-    public ProductServiceUsingCompletableFuture(ProductInfoService productInfoService, ReviewService reviewService) {
+    private final InventoryService inventoryService;
+
+    public ProductServiceUsingCompletableFuture(ProductInfoService productInfoService, ReviewService reviewService, InventoryService inventoryService) {
         this.productInfoService = productInfoService;
         this.reviewService = reviewService;
+        this.inventoryService = inventoryService;
     }
 
     public Product retrieveProductDetails(String productId) {
-        stopWatch.start();
-        CompletableFuture<ProductInfo> productInfoCompletableFuture = CompletableFuture.supplyAsync(() ->  productInfoService.retrieveProductInfo(productId));
-        CompletableFuture<Review> reviewCompletableFuture = CompletableFuture.supplyAsync(() ->  reviewService.retrieveReviews(productId));
+        resetAndStart();
+        CompletableFuture<ProductInfo> productInfoCompletableFuture = CompletableFuture.supplyAsync(() -> productInfoService.retrieveProductInfo(productId));
+        CompletableFuture<Review> reviewCompletableFuture = CompletableFuture.supplyAsync(() -> reviewService.retrieveReviews(productId));
         Product product = productInfoCompletableFuture.thenCombine(reviewCompletableFuture, (productInfo, review) -> new Product(productId, productInfo, review)).join();
         timeTaken();
         return product;
     }
 
+    public Product retrieveProductDetailsWithInventory(String productId) {
+       resetAndStart();
+        CompletableFuture<ProductInfo> productInfoCompletableFuture = CompletableFuture
+                .supplyAsync(() -> productInfoService.retrieveProductInfo(productId))
+                .thenApply(this::updateInventoryInfo);
+        CompletableFuture<Review> reviewCompletableFuture = CompletableFuture.supplyAsync(() -> reviewService.retrieveReviews(productId));
+        Product product = productInfoCompletableFuture.thenCombine(reviewCompletableFuture, (productInfo, review) -> new Product(productId, productInfo, review)).join();
+        timeTaken();
+        return product;
+    }
+
+    private ProductInfo updateInventoryInfo(ProductInfo productInfo) {
+        List<ProductOption> productOptionList =
+                productInfo
+                        .getProductOptions()
+                        //.stream()
+                        .parallelStream()
+                        .peek(productOption -> productOption.setInventory(inventoryService.addInventory(productOption)))
+                        .toList();
+        productInfo.setProductOptions(productOptionList);
+        return productInfo;
+    }
+
+    public Product retrieveProductDetailsWithInventoryApproach2(String productId) {
+        resetAndStart();
+        CompletableFuture<ProductInfo> productInfoCompletableFuture = CompletableFuture
+                .supplyAsync(() -> productInfoService.retrieveProductInfo(productId))
+                .thenApply(result -> {
+                    List<ProductOption> productOptions = updateInventoryInfoApproach2(result);
+                    result.setProductOptions(productOptions);
+                    return result;
+                });
+        CompletableFuture<Review> reviewCompletableFuture = CompletableFuture.supplyAsync(() -> reviewService.retrieveReviews(productId));
+        Product product = productInfoCompletableFuture.thenCombine(reviewCompletableFuture, (productInfo, review) -> new Product(productId, productInfo, review)).join();
+        timeTaken();
+        return product;
+    }
+
+    private List<ProductOption> updateInventoryInfoApproach2(ProductInfo productInfo) {
+        List<CompletableFuture<ProductOption>> completableFutureList = productInfo
+                .getProductOptions()
+                .stream()
+                .map(productOption -> CompletableFuture
+                        .supplyAsync(() -> inventoryService.addInventory(productOption))
+                        .thenApply((inventory) -> {
+                                    productOption.setInventory(inventory);
+                                    return productOption;
+                                }
+                        )).toList();
+        return completableFutureList.stream().map(CompletableFuture::join).toList();
+    }
+
     public CompletableFuture<Product> retrieveProductDetailsApproach2(String productId) {
-        CompletableFuture<ProductInfo> productInfoCompletableFuture = CompletableFuture.supplyAsync(() ->  productInfoService.retrieveProductInfo(productId));
-        CompletableFuture<Review> reviewCompletableFuture = CompletableFuture.supplyAsync(() ->  reviewService.retrieveReviews(productId));
+        CompletableFuture<ProductInfo> productInfoCompletableFuture = CompletableFuture.supplyAsync(() -> productInfoService.retrieveProductInfo(productId));
+        CompletableFuture<Review> reviewCompletableFuture = CompletableFuture.supplyAsync(() -> reviewService.retrieveReviews(productId));
         return productInfoCompletableFuture.thenCombine(reviewCompletableFuture, (productInfo, review) -> new Product(productId, productInfo, review));
     }
 }
--- src/test/java/com/jl/service/completableFuture/ProductServiceUsingCompletableFutureTest.java
+++ src/test/java/com/jl/service/completableFuture/ProductServiceUsingCompletableFutureTest.java
@@ -1,18 +1,21 @@
 package com.jl.service.completableFuture;
 
 import com.jl.domain.Product;
+import com.jl.service.InventoryService;
 import com.jl.service.ProductInfoService;
 import com.jl.service.ReviewService;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ProductServiceUsingCompletableFutureTest {
 
     ProductInfoService productInfoService = new ProductInfoService();
     ReviewService reviewService = new ReviewService();
+    InventoryService inventoryService = new InventoryService();
 
-    ProductServiceUsingCompletableFuture productService = new ProductServiceUsingCompletableFuture(productInfoService, reviewService);
+    ProductServiceUsingCompletableFuture productService = new ProductServiceUsingCompletableFuture(productInfoService, reviewService, inventoryService);
 
     @Test
     public void test_product_service_using_completable_future() {
@@ -23,6 +26,7 @@ public class ProductServiceUsingCompletableFutureTest {
 
         assertEquals(product.getProductInfo().getProductOptions().size(), 2);
         assertEquals(product.getProductInfo().getProductOptions().get(0).getSize(), "64GB");
+        assertNull(product.getProductInfo().getProductOptions().get(0).getInventory());
 
     }
 
@@ -36,6 +40,28 @@ public class ProductServiceUsingCompletableFutureTest {
             assertEquals(product.getProductInfo().getProductOptions().get(0).getSize(), "64GB");
         })).join();
 
+    }
+
+    @Test
+    public void test_product_service_with_inventory_info(){
+        String productId = "testProductId";
+
+        Product product = productService.retrieveProductDetailsWithInventory(productId);
+
+        assertEquals(product.getProductInfo().getProductOptions().size(), 2);
+        assertEquals(product.getProductInfo().getProductOptions().get(0).getSize(), "64GB");
+        assertEquals(product.getProductInfo().getProductOptions().get(0).getInventory().getCount(), 2);
+    }
+
+    @Test
+    public void test_product_service_with_inventory_info_approach_2(){
+        String productId = "testProductId";
+
+        Product product = productService.retrieveProductDetailsWithInventoryApproach2(productId);
+
+        assertEquals(product.getProductInfo().getProductOptions().size(), 2);
+        assertEquals(product.getProductInfo().getProductOptions().get(0).getSize(), "64GB");
+        assertEquals(product.getProductInfo().getProductOptions().get(0).getInventory().getCount(), 2);
     }
 
 }
